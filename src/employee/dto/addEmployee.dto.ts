446852015ef5876b60376f37ee76edc922027725
nwokoyepraise <nwokoyepraise@gmail.com>
2022-05-19 21:43:04 +0100

    add validation packages and addEmployee dto

---
+++ src/employee/dto/addEmployee.dto.ts
@@ -0,0 +1,56 @@
+import { Type } from 'class-transformer';
+import {
+  IsDate,
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+
+class Address {
+  @IsNotEmpty()
+  @IsString()
+  readonly city: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  readonly zipCode : number;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly addressLine1 : string;
+
+  @IsString()
+  @IsOptional()
+  readonly addressLine2 : string;
+}
+
+export class AddEmployeeDto {
+  @IsNotEmpty()
+  @IsString()
+  readonly name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email: string;
+
+  @IsNotEmpty()
+  @IsPhoneNumber()
+  readonly phone: number;
+
+  @IsNotEmpty()
+  @Type(() => Address)
+  @ValidateNested()
+  readonly homeaddress: Address;
+
+  @IsNotEmpty()
+  @IsDate()
+  readonly doe: Date; //date of employment
+
+  @IsNotEmpty()
+  @IsDate()
+  readonly dob: Date; //date of birth
+}
