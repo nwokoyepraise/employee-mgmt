d5669eba07e959b167e15471ba4f758afde41ff8
nwokoyepraise <nwokoyepraise@gmail.com>
2022-05-20 07:22:05 +0100

    add employee add function

--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { AddEmployeeDto } from './dto/addEmployee.dto';
+import { Employee, EmployeeDocument } from './schemas/employee.schema';
 
 @Injectable()
-export class EmployeeService {}
+export class EmployeeService {
+    constructor(@InjectModel(Employee.name) private employeeModel: Model<EmployeeDocument>){}
+
+    async add(addEmployeeDto: AddEmployeeDto): Promise<Employee> {
+        const addedEmployee = new this.employeeModel(addEmployeeDto);
+        return addedEmployee.save();
+    }
+}
