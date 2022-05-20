61cecae0427910cfc85841ef669fdcb43e3f7e5c
nwokoyepraise <nwokoyepraise@gmail.com>
2022-05-20 07:23:14 +0100

    import employee schema

--- src/employee/employee.module.ts
+++ src/employee/employee.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EmployeeService } from './employee.service';
 import { EmployeeController } from './employee.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Employee, EmployeeSchema } from './schemas/employee.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{name: Employee.name, schema: EmployeeSchema}])],
   providers: [EmployeeService],
   controllers: [EmployeeController]
 })
