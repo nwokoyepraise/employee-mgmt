e7cb37c33c7d20e0d7f575bf0273e9f14582591a
nwokoyepraise <nwokoyepraise@gmail.com>
2022-05-21 00:18:45 +0100

    remove unused import

--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
 import { EmployeeDto } from './dto/Employee.dto';
 import { EmployeeRepository } from './employee.repository';
-import { Employee, EmployeeDocument } from './schemas/employee.schema';
+import { Employee} from './schemas/employee.schema';
 
 @Injectable()
 export class EmployeeService {
