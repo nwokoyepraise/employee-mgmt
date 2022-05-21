86bf147b3b8afa3726fa20204eccf0efc26c13e2
nwokoyepraise <nwokoyepraise@gmail.com>
2022-05-21 11:06:41 +0100

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+  .setTitle("Employee Management")
+  .setDescription("Employee Management API Description")
+  .setVersion("1.0")
+  .setContact("nwokoyepraise", "https://github.com/nwokoyepraise", "")
+  .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
