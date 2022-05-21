ab88bfd29cd24b8abd3f4161db839aad6c71e6df
nwokoyepraise <nwokoyepraise@gmail.com>
2022-05-21 10:18:22 +0100

    add hosted mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeeModule } from './employee/employee.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [EmployeeModule,  MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [EmployeeModule, ConfigModule.forRoot(), MongooseModule.forRoot(`mongodb+srv://main:${process.env.MONGO_PASSWORD}@cluster0.j5dr7.mongodb.net/nest?retryWrites=true&w=majority`)],
   controllers: [AppController],
   providers: [AppService],
 })
